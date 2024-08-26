Fixing seeding issue for activation variance experiments

perturbation_to_map.py
@@ -113,7 +113,7 @@ def compute_pairwise_sims(model_set, dataloader=None, layer=1, w_clip=30, a_clip
                                             # names=(names[i], names[j]))
 
             # get the alignments
-            set_seed(COMMON_SEED)
+            #set_seed(COMMON_SEED)
             #simobj.compute_alignments(model1.train_loader, [layer])
             simobj.compute_alignments(dataloader, [layer])
             #simobj.compute_cossim()
@@ -252,7 +252,7 @@ def compute_MDS(similarity_matrix, zero_index=None, pickle=None,
 
     # compute the MDS
     mds = manifold.MDS(n_components=2, dissimilarity='precomputed', eps=1e-16,
-                       max_iter=1000, n_init=100, random_state=0)
+                       max_iter=1000, n_init=100, random_state=0, normalized_stress=False)
     mds.fit_transform(dissims)
     coords = mds.embedding_
 
@@ -555,7 +555,7 @@ def get_variance_axes(coordinates):
     """
     for c in coordinates:
         assert c.shape == coordinates[0].shape, "all coordinates must have the same shape"
-    print(coordinates[0].shape)
+    #print(coordinates[0].shape)
     # collect the corresponding coordinates for each run
     collections = []
     for k in range(coordinates[0].shape[0]):
@@ -580,19 +580,20 @@ def get_variance_axes(coordinates):
     # get the variances :)
     for i in range(len(collections)):
         coll = collections[i]
-        coll_mean = np.array([xs[i], ys[i]])
+        #coll_mean = np.array([xs[i], ys[i]])
+        coll_mean = np.mean(coll, axis=0)
         coll -= coll_mean
         # print('mean-centered coordinates')
         # print(coll)
-        cov = coll.T @ coll # (2, 2)
+        cov = (1/coll.shape[0]) * coll.T @ coll # (2, 2)
 
         # print('covariance')
         # print(cov)
         vals, vecs = np.linalg.eigh(cov)
-        vals, vecs = np.flip(vals, axis=-1), np.flip(vecs, axis=-1)
+        vals, vecs = np.flip(vals), np.flip(vecs, axis=1)
 
         print('eigenvalues : ', vals)
-        print('SD          : ', np.sqrt(vals))
+        # print('SD          : ', np.sqrt(vals))
 
         # append to the correct list
         # directions
@@ -902,52 +903,84 @@ class VariancePlot:
         return plot_info
     
 
-    def plot_variance(self):
+    def plot_variance(self, title=None, variance=True, ylog=False, xlog=False,
+                      ticks=None, ticklabs=None, xlab='Step', xrot=0):
         """
         PLot the change in variance over the course of the trajectories
 
         """
         fig = plt.figure(figsize=(10,5))
 
-        pc1 = [k**2 for k in self.plot_info['s1']]
-        pc2 = [k**2 for k in self.plot_info['s2']]
+        if variance:
+            pc1 = [k**2 for k in self.plot_info['s1']]
+            pc2 = [k**2 for k in self.plot_info['s2']]
+        else:
+            pc1 = self.plot_info['s1']
+            pc2 = self.plot_info['s2']
+        
+        if ticks is None:
+            ticks = list(range(len(pc2)))
 
-        plt.plot(range(len(pc1)), pc1, label='PC1', marker='o', linewidth=0.5)
-        plt.plot(range(len(pc2)), pc2, label='PC2', marker='o', linewidth=0.5)
+        plt.plot(ticks, pc1, label='PC1', marker='o', linewidth=0.5)
+        plt.plot(ticks, pc2, label='PC2', marker='o', linewidth=0.5)
 
-        plt.title('Variance over Trajectory', fontsize=16)
-        plt.xlabel('Step', fontsize=16)
-        plt.ylabel('Variance', fontsize=16)
+        if title is None:
+            if variance:
+                title = 'Variance over Trajectory'
+            else:
+                title = 'Standard Deviation over Trajectory'
+        plt.title(title, fontsize=16)
+        plt.xlabel(xlab, fontsize=16)
+
+        ylab = 'Variance' if variance else 'Standard Deviation'
+        plt.ylabel(ylab, fontsize=16)
+        if ylog:
+            plt.yscale('log')
+        if xlog:
+            plt.xscale('log')
         plt.tick_params(axis='both', which='both', labelsize=16)
-        plt.xticks(list(range(len(pc2))))
+
+
+        if ticklabs is None:
+            ticklabs = ticks
+        plt.xticks(ticks, ticklabs)
 
         plt.legend(fontsize=16)
+        plt.tick_params(axis='x', labelrotation=xrot)
 
         plt.show()
 
         return
     
 
-def plot_variance_plot(coordinates, plot_info=None, title="Variace Plot", sd_mult=2,
+def plot_variance_plot(coordinates, plot_info=None, title="Variance Plot", sd_mult=2,
                        n_models=None, labels=None, increments=None, 
                        text_locs=None, colors=None, legend_cols=2, 
-                       legend_order=None, markers=None, accuracies=None, 
+                       legend_order=None, markers=None, markersize=5,
+                       legend_loc='best',
+
+                       accuracies=None, 
                        bar_range=(0,1), color_traj=None, steps=None, 
                        cb_norm=None, zero_incs=[0], figsize=(12, 10), 
                        zero_sep=False, zero_lab=None, zero_color='red', 
+
                        align_coords=True, yflip=False, xflip=False, zero_ind=None,
+
                        save=False, saveloc='../image_hold', 
-                       xlim=None, ylim=None, xrot=0, varwidth=1e-3, 
+                       xlim=None, ylim=None, xrot=0, 
+                       varwidth=1e-3, 
 
                        mean_color='k', varcols=('#757575', '#757575'),
-                       mean_symbol='D'
+                       mean_symbol='D', mean_mark_size=5,
+                       mean_lab='Mean Trajectory'
     ):
     """
     :param plot_info - output from get_variance_axes
     """
     # align the coordinates if they need to be aligned
-    coordinates = align_traj_to_x(coordinates, yflip=yflip, xflip=xflip, 
-                                 zero_ind=zero_ind)
+    if align_coords:
+        coordinates = align_traj_to_x(coordinates, yflip=yflip, xflip=xflip, 
+                                    zero_ind=zero_ind)
 
     """If there is plotinfo for variance"""
     if plot_info is not None:
@@ -971,27 +1004,32 @@ def plot_variance_plot(coordinates, plot_info=None, title="Variace Plot", sd_mul
             if k != 0: 
                 s1.append(1/(k * sd_mult) )
             else: 
-                s1.append(0)
+                s1.append(np.inf)
         s2 = []
         for k in plot_info['s2']:
             if k != 0: 
                 s2.append(1/(k * sd_mult)) 
             else: 
-                s2.append(0)
+                s2.append(np.inf)
         
         mean_coords = np.array([xs_in, ys_in]).T
+        #print(mean_coords)
 
         # align the coordinates if true and flip as indicated
         if align_coords:
             mean_coords = align_traj_to_x(mean_coords, yflip=yflip, xflip=xflip, 
-                                    zero_ind=zero_ind)
+                                   zero_ind=zero_ind)
             # adjusting the axes as necessary
             if yflip:
                 u1 = [-k for k in u1]
                 u2 = [-k for k in u2]
+                # v1 = [-k for k in v1]
+                # v2 = [-k for k in v2]
             if xflip:
                 v1 = [-k for k in v1]
                 v2 = [-k for k in v2]
+                # u1 = [-k for k in u1]
+                # u2 = [-k for k in u2]
     
     # set the default color map
     color_map = plt.cm.plasma  
@@ -1016,7 +1054,7 @@ def plot_variance_plot(coordinates, plot_info=None, title="Variace Plot", sd_mul
     if n_models:
         split_indices = [0]+[sum(n_models[:i]) for i in range(1,len(n_models)+1)]
     else: 
-        split_indices = [0, len(coords) -1]
+        split_indices = [0, len(coordinates) -1]
     
     # number of different trajectories
     n_perturbations = len(n_models) if n_models else 1
@@ -1056,17 +1094,17 @@ def plot_variance_plot(coordinates, plot_info=None, title="Variace Plot", sd_mul
         # plotting the trajectory
         if zero_sep:
             #print(xs, ys)
-            plt.scatter(xs[0], ys[0], marker=mark, s=100, 
+            plt.scatter(xs[0], ys[0], marker=mark, s=markersize**2, 
                         color=zero_color, zorder=5)
             if i == 0:
                 zero_legend = [matplotlib.patches.Patch(facecolor=zero_color,
                                                         edgecolor=zero_color,
                                                         label=zero_lab)]
         if accuracies or color_traj:
-            plt.plot(xs, ys, markersize=10, linestyle=':',
+            plt.plot(xs, ys, markersize=markersize, linestyle=':',
                  color='k', label=labels[i], linewidth=1, zorder=1)
             if accuracies:
-                plt.scatter(xs, ys, c=accuracies[i], marker=mark, s=100,
+                plt.scatter(xs, ys, c=accuracies[i], marker=mark, s=markersize**2,
                             cmap=color_map, vmin=bar_range[0], 
                             vmax=bar_range[1], zorder=2, norm=cb_norm)
                 increment_color = color_map(accuracies[i][-1])
@@ -1076,11 +1114,11 @@ def plot_variance_plot(coordinates, plot_info=None, title="Variace Plot", sd_mul
                     colors[colors <= 0] = 1e10-4
                 #print(colors)
                 plt.scatter(xs, ys, c=step_i, cmap=color_traj, 
-                            marker=mark, s=100, norm=cb_norm,
+                            marker=mark, s=markersize**2, norm=cb_norm,
                             vmin=bar_range[0], vmax=bar_range[1], zorder=2)
                 increment_color = color_traj(step_i[-1])
         else:
-            plt.plot(xs, ys, markersize=10, marker=mark, linestyle=':',
+            plt.plot(xs, ys, markersize=markersize, marker=mark, linestyle=':',
                  color=colors[i], label=labels[i], linewidth=1, zorder=1,
                  mew=0)
             increment_color = colors[i]
@@ -1122,32 +1160,40 @@ def plot_variance_plot(coordinates, plot_info=None, title="Variace Plot", sd_mul
 
     """Plot the means"""
     if plot_info is not None:
+        #print(mean_coords)
         mean_legend = plt.scatter(mean_coords[:,0], mean_coords[:,1], color=mean_color,
-                    marker=mean_symbol, s=100, label='Mean Trajectory')
+                    marker=mean_symbol, s=mean_mark_size**2, label=mean_lab, zorder=10000)
         
         if zero_sep:
-            plt.scatter(mean_coords[0,0], mean_coords[0,1], marker=mean_symbol, s=100, 
-                        color=zero_color, zorder=5)
-            ax.legend(handles=zero_legend+[mean_legend], fontsize=16)
+        #     plt.scatter(mean_coords[0,0], mean_coords[0,1], marker=mean_symbol, s=100, 
+        #                 color=zero_color, zorder=5)
+            ax.legend(handles=zero_legend+[mean_legend], fontsize=16, loc=legend_loc)
 
 
     """ADDING THE ARROWS"""
     # add in the first axis
     for i in range(len(u1)):
         s = s1[i] if s1[i] != 0 else 1
-        qv1 = plt.quiver(mean_coords[i, 0], mean_coords[i, 1], u1[i], v1[i], scale=s, headwidth=0, 
-                   scale_units='x', width=varwidth, color=varcols[0],
-                   label='')
-        plt.quiver(mean_coords[i, 0], mean_coords[i, 1], -u1[i], -v1[i], scale=s, headwidth=0, 
-                   scale_units='x', width=varwidth, color=varcols[0])
+        qv1 = plt.quiver(mean_coords[i, 0], mean_coords[i, 1], u1[i], v1[i], 
+                        scale=s, headwidth=1, headlength=0, 
+                        scale_units='x', units='dots', width=varwidth, 
+                        color=varcols[0],
+                        label='', zorder=10000)
+        plt.quiver(mean_coords[i, 0], mean_coords[i, 1], -u1[i], -v1[i], 
+                    scale=s, headwidth=1, headlength=0,
+                    scale_units='x', units='dots', width=varwidth, 
+                    color=varcols[0], zorder=10000)
     # second axis
     for i in range(len(u2)):
         s = s2[i] if s2[i] != 0 else 1
-        qv2 = plt.quiver(mean_coords[i, 0], mean_coords[i, 1], u2[i], v2[i], scale=s, headwidth=0,
-                   scale_units='x', width=varwidth, color=varcols[1],
-                   label='')
-        plt.quiver(mean_coords[i, 0], mean_coords[i, 1], -u2[i], -v2[i], scale=s, headwidth=0,
-                   scale_units='x', width=varwidth, color=varcols[1])
+        qv2 = plt.quiver(mean_coords[i, 0], mean_coords[i, 1], u2[i], v2[i], 
+                        scale=s, headwidth=1, headlength=0,
+                        scale_units='x', units='dots', width=varwidth, 
+                        color=varcols[1], label='', zorder=10000)
+        plt.quiver(mean_coords[i, 0], mean_coords[i, 1], -u2[i], -v2[i], 
+                        scale=s, headwidth=1, headlength=0,
+                        scale_units='x', units='dots', width=varwidth, 
+                        color=varcols[1], zorder=10000)
 
     # setting the colorbar
     if accuracies or color_traj:
@@ -1191,3 +1237,61 @@ def plot_variance_plot(coordinates, plot_info=None, title="Variace Plot", sd_mul
         plt.savefig(savepath)
     
     return 
+
+
+class VariancePlotEvo:
+
+    def __init__(self, *args, names=None):
+        """
+        :param args     :   these should be VariancePlot objects
+        """
+        self.plot_objs = args
+
+        if names is not None:
+            assert len(args) == len(names), \
+                'Number of names must equal number of trajectories'
+            self.names = names
+        else:
+            self.names = list(range(1, len(args)+1))
+
+        for plot in self.plot_objs:
+            plot.get_variance_plot_info()
+            # now each plot has .plot_info not set to None
+
+        return
+    
+    def plot_variance_evo(self, title=None, tick_locs=None, xlog=False,
+                          xlabel='X'):
+        s_list1 = []
+        s_list2 = []
+        for plot in self.plot_objs:
+            s_list1.append(np.mean([k**2 for k in plot.plot_info['s1']]))
+            s_list2.append(np.mean([k**2 for k in plot.plot_info['s2']]))
+        
+        if tick_locs is None:
+            tick_locs = list(range(len(s_list2)))
+        fig = plt.figure(figsize=(10, 5))
+        plt.plot(tick_locs, s_list1, label='PC1', 
+                marker='o', linewidth=0.5)
+        plt.plot(tick_locs, s_list2, label='PC2',
+                marker='o', linewidth=0.5)
+
+        if title is None:
+            title = 'Variance Evolution'
+        plt.title(title, fontsize=16)
+        plt.xlabel(xlabel, fontsize=16)
+        plt.ylabel('Average Interval Variance', fontsize=16)
+        plt.tick_params(axis='both', which='both', labelsize=16)
+        
+        
+        plt.xticks(tick_locs, self.names)
+
+        if xlog:
+            plt.xscale('log')
+
+        plt.yscale('log')
+        plt.legend(fontsize=16)
+
+        plt.show()
+
+        return